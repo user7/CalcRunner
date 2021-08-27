880c1f03b106536afa6e08f7514e3f62b62bbe4f
svr <svr@protonmail.com>
2021-08-28 02:03:20 +0300

    Пустое приложение

---
+++ app/src/main/java/com/geekbrains/calcrunner/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.geekbrains.calcrunner
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
